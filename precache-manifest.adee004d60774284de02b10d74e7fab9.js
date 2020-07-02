6508ce4e1febcbf7b0961d928eb309382aa67d41
Donchi1 <64812693+Donchi1@users.noreply.github.com>
2020-07-02 11:11:47 +0400

    Updates

---
+++ precache-manifest.adee004d60774284de02b10d74e7fab9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a12de737d2546e2a0ff589d4b5e0113c",
+    "url": "/First-React/index.html"
+  },
+  {
+    "revision": "95c375c77c773d8ba863",
+    "url": "/First-React/static/css/main.75922f2b.chunk.css"
+  },
+  {
+    "revision": "a212f6fa09f4577a23f1",
+    "url": "/First-React/static/js/2.12faf68e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/First-React/static/js/2.12faf68e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95c375c77c773d8ba863",
+    "url": "/First-React/static/js/main.0fdf104a.chunk.js"
+  },
+  {
+    "revision": "ced315300e804e656c88",
+    "url": "/First-React/static/js/runtime-main.6b06afac.js"
+  }
+]);
